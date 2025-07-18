8f91e163e7819bf28e6f65802e6836b70a7c13a0
hanyMamdouh22 <hanymmdouh55@gmail.com>
2025-07-18 23:34:12 +0300

    Add task5 folder

---
+++ task5/database.sql
@@ -0,0 +1,13 @@
+create databse dashboard
+character set ustf8mb4
+collate ustf8mb4_unicide_ci;
+
+
+--create table (users)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
